9c648f6c2d0c8d3d25349ce6e98f1faf422f764a
jakipatryk <bajbartlomiej997@gmail.com>
2018-05-03 13:11:17 +0200

    Set up NGRX router-store and mock AppRoutingModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ServiceWorkerModule } from '@angular/service-worker';
+import { StoreRouterConnectingModule, routerReducer } from '@ngrx/router-store';
 import { StoreModule } from '@ngrx/store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { environment } from '../environments/environment';
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './core/containers/app/app.component';
 import { CoreModule } from './core/core.module';
 
@@ -16,10 +18,14 @@ import { CoreModule } from './core/core.module';
       enabled: environment.production
     }),
     CoreModule.forRoot(),
-    StoreModule.forRoot({}),
+    AppRoutingModule,
+    StoreModule.forRoot({ router: routerReducer }),
     StoreDevtoolsModule.instrument({
       maxAge: 10,
       logOnly: environment.production
+    }),
+    StoreRouterConnectingModule.forRoot({
+      stateKey: 'router'
     })
   ],
   providers: [],
