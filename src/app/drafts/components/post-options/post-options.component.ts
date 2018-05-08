cc85d0b03223549d4c69a59e112519bb366296b0
jakipatryk <bajbartlomiej997@gmail.com>
2018-05-08 12:55:18 +0200

    Add post options component (helper component & presentational component)

---
+++ src/app/drafts/components/post-options/post-options.component.ts
@@ -0,0 +1,29 @@
+import {
+  ChangeDetectionStrategy,
+  Component,
+  EventEmitter,
+  Input,
+  Output
+} from '@angular/core';
+import { FormGroup } from '@angular/forms';
+import { Beneficiary } from './../../models/beneficiary.model';
+
+@Component({
+  selector: 'app-post-options',
+  templateUrl: './post-options.component.html',
+  styleUrls: ['./post-options.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class PostOptionsComponent {
+  @Input() parent: FormGroup;
+  @Output() addBeneficiary = new EventEmitter<Beneficiary>();
+  @Output() removeBeneficiary = new EventEmitter<number>();
+
+  onAddBeneficiary(beneficiary: Beneficiary) {
+    this.addBeneficiary.emit(beneficiary);
+  }
+
+  onRemoveBeneficiary(i: number) {
+    this.removeBeneficiary.emit(i);
+  }
+}
