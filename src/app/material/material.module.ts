404eeb58c64de09d8dbfa6decce6d58bf5723fae
jakipatryk <bajbartlomiej997@gmail.com>
2018-05-08 12:51:56 +0200

    Update MaterialModule

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,13 +1,45 @@
 import { NgModule } from '@angular/core';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatStepperModule } from '@angular/material/stepper';
 import { MatToolbarModule } from '@angular/material/toolbar';
 
 /**
  * Import and export here each Angular Material module that is going to be used in the app.
  */
 @NgModule({
-  imports: [MatButtonModule, MatToolbarModule, MatCardModule],
-  exports: [MatButtonModule, MatToolbarModule, MatCardModule]
+  imports: [
+    MatButtonModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatStepperModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatAutocompleteModule,
+    MatSelectModule,
+    MatSlideToggleModule,
+    MatSliderModule,
+    MatIconModule
+  ],
+  exports: [
+    MatButtonModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatStepperModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatAutocompleteModule,
+    MatSelectModule,
+    MatSlideToggleModule,
+    MatSliderModule,
+    MatIconModule
+  ]
 })
 export class MaterialModule {}
