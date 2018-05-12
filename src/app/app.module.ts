d20842dfdc9bda3af4cdf888950469c1c13bd1a3
jakipatryk <bajbartlomiej997@gmail.com>
2018-05-12 20:22:46 +0200

    Add router state serializer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { Params, RouterStateSnapshot } from '@angular/router';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { EffectsModule } from '@ngrx/effects';
-import { StoreRouterConnectingModule, routerReducer } from '@ngrx/router-store';
-import { StoreModule } from '@ngrx/store';
+import {
+  RouterReducerState,
+  RouterStateSerializer,
+  StoreRouterConnectingModule,
+  routerReducer
+} from '@ngrx/router-store';
+import { ActionReducerMap, StoreModule } from '@ngrx/store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { environment } from '../environments/environment';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './core/containers/app/app.component';
 import { CoreModule } from './core/core.module';
+import { reducers, effects, CustomSerializer } from './store';
 
 @NgModule({
   imports: [
@@ -20,7 +27,7 @@ import { CoreModule } from './core/core.module';
     }),
     CoreModule.forRoot(),
     AppRoutingModule,
-    StoreModule.forRoot({ router: routerReducer }),
+    StoreModule.forRoot(reducers),
     StoreDevtoolsModule.instrument({
       maxAge: 10,
       logOnly: environment.production
@@ -30,6 +37,7 @@ import { CoreModule } from './core/core.module';
     }),
     EffectsModule.forRoot([])
   ],
+  providers: [{ provide: RouterStateSerializer, useClass: CustomSerializer }],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
