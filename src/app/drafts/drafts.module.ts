7a1d5bc1f6ebf3d43c158cc305bcb26f932ed1b5
jakipatryk <bajbartlomiej997@gmail.com>
2018-05-15 02:23:21 +0200

    Update `Drafts` module

--- src/app/drafts/drafts.module.ts
+++ src/app/drafts/drafts.module.ts
@@ -2,32 +2,45 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { ReactiveFormsModule } from '@angular/forms';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreModule } from '@ngrx/store';
 import { MaterialModule } from './../material/material.module';
 import { BeneficiaryFormComponent } from './components/beneficiary-form/beneficiary-form.component';
+import { DraftCardComponent } from './components/draft-card/draft-card.component';
 import { EditorComponent } from './components/editor/editor.component';
+import { NewDraftDialogComponent } from './components/new-draft-dialog/new-draft-dialog.component';
 import { PostContentsComponent } from './components/post-contents/post-contents.component';
 import { PostOptionsComponent } from './components/post-options/post-options.component';
 import { PostThumbnailComponent } from './components/post-thumbnail/post-thumbnail.component';
-import { TemplatePickerComponent } from './components/template-picker/template-picker.component';
-import { DraftComponent } from './containers/draft/draft.component';
+import { DraftsComponent } from './containers/drafts/drafts.component';
 import { DraftsRoutingModule } from './drafts-routing.module';
+import { effects } from './store/effects';
+import { reducers } from './store/reducers';
+import { ReversePipe } from './pipes/reverse.pipe';
+import { EditorContainerComponent } from './containers/editor-container/editor-container.component';
 
 @NgModule({
   imports: [
     CommonModule,
     ReactiveFormsModule,
     DraftsRoutingModule,
+    StoreModule.forFeature('drafts', reducers),
+    EffectsModule.forFeature(effects),
     MaterialModule,
     FlexLayoutModule
   ],
   declarations: [
-    DraftComponent,
+    DraftsComponent,
     EditorComponent,
-    TemplatePickerComponent,
     PostContentsComponent,
     PostThumbnailComponent,
     PostOptionsComponent,
-    BeneficiaryFormComponent
-  ]
+    BeneficiaryFormComponent,
+    DraftCardComponent,
+    NewDraftDialogComponent,
+    ReversePipe,
+    EditorContainerComponent
+  ],
+  entryComponents: [NewDraftDialogComponent]
 })
 export class DraftsModule {}
