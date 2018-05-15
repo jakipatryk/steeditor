7700b5d98cf5c163b2a8b17e74f9d6aea1cf8102
jakipatryk <bajbartlomiej997@gmail.com>
2018-05-15 02:23:01 +0200

    Add `Editor` component (container)

---
+++ src/app/drafts/containers/editor-container/editor-container.component.ts
@@ -0,0 +1,26 @@
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs/Observable';
+import * as fromStore from '../../store';
+import { Draft } from './../../models/draft.model';
+
+@Component({
+  selector: 'app-editor-container',
+  templateUrl: './editor-container.component.html',
+  styleUrls: ['./editor-container.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class EditorContainerComponent implements OnInit {
+  draft$: Observable<Draft>;
+
+  constructor(private store: Store<fromStore.State>) {}
+
+  ngOnInit() {
+    this.draft$ = this.store.select(fromStore.selectCurrentDraft);
+  }
+
+  updateDraft(draft: Draft) {
+    console.log(draft);
+    this.store.dispatch(fromStore.updateDraft(draft));
+  }
+}
