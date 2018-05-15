e5fcc0f618bd40408af06c66aafdf87e831dc3ce
jakipatryk <bajbartlomiej997@gmail.com>
2018-05-15 02:23:40 +0200

    Update routes for `Drafts` module

--- src/app/drafts/drafts-routing.module.ts
+++ src/app/drafts/drafts-routing.module.ts
@@ -1,11 +1,20 @@
-import { DraftComponent } from './containers/draft/draft.component';
+import { EditorContainerComponent } from './containers/editor-container/editor-container.component';
+import { DraftsComponent } from './containers/drafts/drafts.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const draftsRoutes: Routes = [
   {
     path: '',
-    component: DraftComponent
+    component: DraftsComponent
+  },
+  {
+    path: 'editor',
+    component: EditorContainerComponent
+  },
+  {
+    path: ':draftId',
+    component: EditorContainerComponent
   }
 ];
 
