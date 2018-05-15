e141097394c50ddef7270ade0e6b219d346c4fdf
jakipatryk <bajbartlomiej997@gmail.com>
2018-05-15 02:25:12 +0200

    Fix wrongly imported Effects in the root module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { Params, RouterStateSnapshot } from '@angular/router';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { EffectsModule } from '@ngrx/effects';
 import {
-  RouterReducerState,
   RouterStateSerializer,
-  StoreRouterConnectingModule,
-  routerReducer
+  StoreRouterConnectingModule
 } from '@ngrx/router-store';
-import { ActionReducerMap, StoreModule } from '@ngrx/store';
+import { StoreModule } from '@ngrx/store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { environment } from '../environments/environment';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './core/containers/app/app.component';
 import { CoreModule } from './core/core.module';
-import { reducers, effects, CustomSerializer } from './store';
+import { CustomSerializer, effects, reducers } from './store';
 
 @NgModule({
   imports: [
@@ -35,7 +32,7 @@ import { reducers, effects, CustomSerializer } from './store';
     StoreRouterConnectingModule.forRoot({
       stateKey: 'router'
     }),
-    EffectsModule.forRoot([])
+    EffectsModule.forRoot(effects)
   ],
   providers: [{ provide: RouterStateSerializer, useClass: CustomSerializer }],
   bootstrap: [AppComponent]
