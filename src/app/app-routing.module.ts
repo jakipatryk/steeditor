068d064d3f58414e2ce846d579296448a675f76c
jakipatryk <bajbartlomiej997@gmail.com>
2018-05-19 01:59:08 +0200

    Add a route for SteemConnect redirect

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RedirectComponent } from './core/containers/redirect/redirect.component';
 
 const appRoutes: Routes = [
   {
     path: 'drafts',
     loadChildren: './drafts/drafts.module#DraftsModule'
+  },
+  {
+    path: 'steemconnect/redirect',
+    component: RedirectComponent
   }
 ];
 
