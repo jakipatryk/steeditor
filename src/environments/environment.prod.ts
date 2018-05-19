0c89a4770808876f35e28ee8f6e389a3cfbce83b
jakipatryk <bajbartlomiej997@gmail.com>
2018-05-19 02:11:43 +0200

    Import and configurate AuthModule

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,8 @@
 export const environment = {
-  production: true
+  production: true,
+  steemConnectConfig: {
+    clientId: '<client-id-of-the-steemconnect-app>',
+    redirectUrl: '<redirect-uri-for-production>',
+    scope: ['comment', 'comment_options']
+  }
 };
