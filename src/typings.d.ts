462e859aa99ac789806b01314acc9930a9a3bf8d
jakipatryk <bajbartlomiej997@gmail.com>
2018-06-26 23:00:23 +0200

    Enable importing JSON

--- src/typings.d.ts
+++ src/typings.d.ts
@@ -1,5 +1,10 @@
 /* SystemJS module definition */
 declare var module: NodeModule;
+
+declare module '*.json' {
+  const value: any;
+  export default value;
+}
 interface NodeModule {
   id: string;
 }
