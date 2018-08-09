6142a31ac2a7c2eda4bca6be704f782421485d05
jakipatryk <bajbartlomiej997@gmail.com>
2018-08-09 14:16:19 +0200

    fix(idb): Change Store creation strategy (#48)
    - hardcode available Stores in the IndexedDBService as a type
    - allow only those Stores to be used
    - add comments how to add a new Store

--- src/app/core/services/indexeddb.service.ts
+++ src/app/core/services/indexeddb.service.ts
@@ -2,27 +2,35 @@ import { Injectable } from '@angular/core';
 import idb, { DB } from 'idb';
 import { from, Observable } from 'rxjs';
 
+// To add a new store you should (in this file):
+//   1. Add this Store name to `Stores` type.
+//   2. Increment version.
+//   3. Add condition which checks if this Store already exists and if not create one.
+
+// (1) Add the Store name for each Store you want to use.
+/** Stores available in the application's IndexedDB. */
+export type Stores = 'drafts';
+
+/** Simple service used to deal with IndexedDB. */
 @Injectable({
   providedIn: 'root'
 })
 export class IndexedDBService {
-  private storeName;
-  // Bump the version each time you want to create new Store.
-  private dbVersion = 1;
+  // (2) Increment the version each time you want to add a new Store.
+  private readonly dbVersion = 1;
   private readonly dbName = 'steeditor-db';
-
-  constructor() {}
+  private storeName: Stores;
 
   /**
-   * Sets Store name to be used in all other operations.
+   * Sets Store to be used in all other operations.
    */
-  public useStore(name: string): void {
+  public useStore(name: Stores): void {
     this.storeName = name;
   }
 
   public getOne<T>(key: IDBValidKey): Observable<T> {
     return from(
-      this.createStore().then(db =>
+      this.openDB().then(db =>
         db
           .transaction(this.storeName, 'readonly')
           .objectStore(this.storeName)
@@ -33,7 +41,7 @@ export class IndexedDBService {
 
   public getAll<T>(): Observable<T[]> {
     return from(
-      this.createStore().then(db =>
+      this.openDB().then(db =>
         db
           .transaction(this.storeName, 'readonly')
           .objectStore(this.storeName)
@@ -44,7 +52,7 @@ export class IndexedDBService {
 
   public add(value: any): Observable<IDBValidKey> {
     return from(
-      this.createStore().then(db =>
+      this.openDB().then(db =>
         db
           .transaction('drafts', 'readwrite')
           .objectStore('drafts')
@@ -55,7 +63,7 @@ export class IndexedDBService {
 
   public put(value: any): Observable<IDBValidKey> {
     return from(
-      this.createStore().then(db =>
+      this.openDB().then(db =>
         db
           .transaction('drafts', 'readwrite')
           .objectStore('drafts')
@@ -66,7 +74,7 @@ export class IndexedDBService {
 
   public delete(key: IDBValidKey): Observable<void> {
     return from(
-      this.createStore().then(db =>
+      this.openDB().then(db =>
         db
           .transaction('drafts', 'readwrite')
           .objectStore('drafts')
@@ -75,10 +83,11 @@ export class IndexedDBService {
     );
   }
 
-  private createStore(): Promise<DB> {
+  private openDB(): Promise<DB> {
     return idb.open(this.dbName, this.dbVersion, upgradeDb => {
-      if (!upgradeDb.objectStoreNames.contains(this.storeName)) {
-        upgradeDb.createObjectStore(this.storeName, {
+      // (3) Add condition for each Store to make sure this Store exists.
+      if (!upgradeDb.objectStoreNames.contains('drafts')) {
+        upgradeDb.createObjectStore('drafts', {
           keyPath: 'id',
           autoIncrement: true
         });
